5e203be9c0d6edac04f99d2f5d036e571c77e513
Tom Manion <thomaswmanion@gmail.com>
2015-10-07 23:48:25 -0700

    First NG Commit

---
+++ src/main.module.js
@@ -0,0 +1,18 @@
+'use strict';
+var angular = require('angular');
+
+angular.module('app', [
+	require('angular-ui-router')
+]).config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('home', {
+		url: '/',
+		controller: 'HomeController',
+		controllerAs: 'home',
+		template: '<h1>{{home.message}}</h1>'
+	});
+	$urlRouterProvider.otherwise('/');
+}).controller('HomeController', function() {
+	this.message = 'Hello World!';
+});
+
+module.exports = 'asdfasdf';
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+var webpack = require('webpack');
+var glob = require('glob');
+var path = require('path');
+var prod = process.env.prod;
+
+//Variables
+var plugins = [];
+var dest;
+
+//Setup Webpack
+if (prod) {
+    var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+    plugins.push(new ngAnnotatePlugin({
+        add: true
+    }));
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        minimize: true
+    }));
+    dest = path.join(__dirname, 'dist')
+}
+else {
+    dest = path.join(__dirname, '.tmp');
+}
+module.exports = {
+    context: __dirname,
+    devServer: {
+        historyApiFallback: true,
+        port: 9000,
+        contentBase: './src'
+    },
+    entry: {
+        app: glob.sync("./src/*{,*/*}.module.js")
+    },
+    output: {
+        path: dest,
+        filename: 'scripts.js'
+    },
+    resolve: {
+        root: './bower_components'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel'
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css!sass'
+        }]
+    },
+    plugins: plugins
+};
+
+
+//Setup build
+if (prod) {
+    var err = function(err) {
+        if (err) throw console.error(err);
+    };
+    var fs = require('fs-extra');
+    var minify = require('html-minifier').minify;
+
+    fs.emptyDir('./dist', function(err) {
+        if (err) throw console.error(err);
+        fs.readFile('./src/index.html', 'utf8', function(err, data) {
+            if (err) throw console.error(err);
+            var result = minify(data, {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                removeOptionalTags: true
+            });
+            fs.writeFile('./dist/index.html', result, err);
+        });
+    })
+
+
+}
